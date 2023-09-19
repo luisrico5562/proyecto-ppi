56b0bb6e9cee7d08dce30d7df61685449aca30c0
samuj02 <samuel.jimenez8643@alumnos.udg.mx>
2023-09-19 16:02:47 -0600

    Creación proyecto y migraciones

---
+++ app/Http/Controllers/DiscoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Disco;
+use Illuminate\Http\Request;
+
+class DiscoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Disco $disco)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Disco $disco)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Disco $disco)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Disco $disco)
+    {
+        //
+    }
+}
